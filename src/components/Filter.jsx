f9f5349464222baf91d6648b9ab66a0b87ad87a6
facchin21 <130471266+facchin21@users.noreply.github.com>
2024-06-22 12:18:14 -0300

    Agrego funcion de Filtrar por colores(Falta terminar, tiene un error de estados CREO).

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,35 +1,47 @@
 /* eslint-disable react/prop-types */
 
-export const Filter = ({ toggleFilter }) => {
-  const colors = ['Negro', "Blanco", "Marron"]
-  const cajaRegalo = ['Si', 'No']
-    return (
+import { useEffect, useState } from "react";
+
+export const Filter = ({ toggleFilter, productos, handleFilter }) => {
+  const [colors, setColors] = useState([]);
+  
+  useEffect(() => {
+    const allColors = productos.reduce((acc, item) => {
+      item.colors.forEach(color => {
+        if (!acc.includes(color)) {
+          acc.push(color);
+        }
+      });
+      return acc;
+    }, []);
+    
+    setColors(allColors);
+  }, [productos]);
+
+  const filterByColor = (color) => {
+    const resultColor = productos.filter(productColors => productColors.colors.includes(color));
+    handleFilter(resultColor);
+  };
+
+  return (
     <div className="flex flex-col py-12 px-6 bg-detail w-48 h-screen absolute barra__filtro">
-        <button className='btn__menu' onClick={toggleFilter}>
-            <div className='btn__menu-x'></div>
-            <div className='btn__menu-x'></div>
-        </button>
-        <div className="py-8 font-League">
-            <h3 className="text-xl font-medium">Color</h3>
-            {colors.map((item, key)=>(
-                <div key={key} className="flex gap-2 items-center">
-                    <input type="checkbox" 
-                    className="custom-checkbox"/>
-                    <span>{item}</span>
-                </div>
-            ))}
-        </div>
-        <div>
-            <h3 className="text-xl font-medium">
-                Caja de regalo
-            </h3>
-            {cajaRegalo.map((item, key)=>(
-                <div key={key} className="flex gap-2 items-center">
-                    <input type="checkbox" className="custom-checkbox"/>
-                    <span>{item}</span>
-                </div>
-            ))}
-        </div>
+      <button className='btn__menu' onClick={toggleFilter}>
+        <div className='btn__menu-x'></div>
+        <div className='btn__menu-x'></div>
+      </button>
+      <header>
+        <h3 className="text-xl font-semibold">Filtros</h3>
+      </header>
+      <div className="py-8 font-League">
+        <h3 className="text-lg font-medium">Color</h3>
+        {colors.map((item, key) => (
+          <div key={key} className="flex gap-2 items-center">
+            <input type="checkbox" 
+                   className="custom-checkbox" onClick={() => filterByColor(item)} />
+            <span className="capitalize">{item}</span>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
